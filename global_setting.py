5268f15a709eaa96607bc188710c175683262439
shaoguangleo <shaoguangleo@gmail.com>
2015-08-22 00:24:24 +0800

    Add more macro

--- global_setting.py
+++ global_setting.py
@@ -15,8 +15,13 @@ AstroSoft_Src       = AstroSoft_Home+'/src'
 AstroSoft_pgplot    = AstroSoft_Home+'/pgplot'
 AstroSoft_tempo     = AstroSoft_Home+'/tempo'
 AstroSoft_tempo2    = AstroSoft_Home+'/tempo2'
+AstroSoft_sigproc   = AstroSoft_Home+'/sigproc'
 AstroSoft_presto    = AstroSoft_Home+'/presto'
 AstroSoft_psrcat    = AstroSoft_Home+'/psrcat'
+AstroSoft_cfitsio   = AstroSoft_Home+'/cfitsio'
+AstroSoft_calceph   = AstroSoft_Home+'/calceph'
+AstroSoft_psrchive  = AstroSoft_Home+'/psrchive'
+AstroSoft_dspsr     = AstroSoft_Home+'/dspsr'
 
-PreUbuntu_software = ('build-essential autoconf automake libtool git cvs swig g++ gfortran python-pip libatlas-* libfftw3-* libcfitsio3* liblapack* python-dev')
+PreUbuntu_software = ('build-essential autoconf automake libtool git cvs swig g++ gfortran python-pip libatlas-* libfftw3-* libcfitsio3* liblapack* python-dev csh')
 PrePip_software =('ipython numpy scipy matplotlib')

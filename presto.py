ed13972fbb16b5cd7df190f3705c888ce7ba7516
shaoguangleo <shaoguangleo@gmail.com>
2015-08-26 12:39:46 +0800

    Add make makewisdom & make prep from Yu Qingzheng

--- presto.py
+++ presto.py
@@ -32,6 +32,8 @@ def install_presto():
     os.system('tar zxvf '+ presto_name)
     os.chdir('presto/src')
     #install_package.install_package(global_setting.AstroSoft_presto,presto_name,configure_option,4)
+    os.system('make makewisdom')
+    os.system('make prep')
     os.system('make')
     os.system('make install')
 def setting_presto():

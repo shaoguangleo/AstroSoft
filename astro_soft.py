be69c0e87d46ad69b09671918e70c1753cf1d144
shaoguangleo <shaoguangleo@gmail.com>
2016-04-19 19:42:54 +0800

    First init the main program

---
+++ astro_soft.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+__author__ = "Guo Shaoguang<sgguo@shao.ac.cn>"
+__version__ = 1.0
+__date__ = "2016-04-18"
+
+
+__all__ = ['list','install']
+
+if __name__ == '__main__':
+    pass
--- sep.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# This file just for beautiful format, no meaning
-__author__ = "Guo Shaoguang<sgguo@shao.ac.cn>"
-__version__ = 1.0
-__date__ = "2015-08-18"
-def sep():
-    print '-'*75

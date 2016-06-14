27e01e1cfd090248c8c225f08c30bdec13bc4382
shaoguangleo <shaoguangleo@gmail.com>
2016-06-14 12:24:03 +0800

    Adding AstroSoft in introduction

---
+++ script/install_difx_ubuntu_online.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+#     _        _            ____         __ _
+#    / \   ___| |_ _ __ ___/ ___|  ___  / _| |_
+#   / _ \ / __| __| '__/ _ \___ \ / _ \| |_| __|
+#  / ___ \\__ \ |_| | | (_) |__) | (_) |  _| |_
+# /_/   \_\___/\__|_|  \___/____/ \___/|_|  \__|
+#
+#
+#
+
+# Test on Ubuntu 10.04 with DiFX2.0.1
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install build-essential pkg-config \
+                      bison flex libfftw3-dev libopenmpi-dev openmpi-bin\
+                      automake gfortran libexpat1-dev \
+                      subversion doxygen openmpi-bin automake autoconf libtool
+
+# Install Intel IPP

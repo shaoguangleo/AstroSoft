7b165cb3091e390afce80a6711791e4e914a5952
shaoguangleo <shaoguangleo@gmail.com>
2016-06-17 16:19:06 +0800

    Adding some structions when install DiFX

--- script/install_difx_ubuntu_online.sh
+++ script/install_difx_ubuntu_online.sh
@@ -15,9 +15,32 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install build-essential pkg-config \
-                      bison flex libfftw3-dev libopenmpi-dev openmpi-bin\
-                      automake gfortran libexpat1-dev \
+## On Lucid - Ubuntu 10.04
+sudo apt-get install build-essential pkg-config bison flex libfftw3* openmpi-bin\
+                      automake gfortran libexpat1-dev  libopenmpi*\
                       subversion doxygen openmpi-bin automake autoconf libtool
 
+## on Natty & Precise (12.04)
+sudo apt-get install build-essential pkg-config bison flex libfftw3-dev libopenmpi-dev \
+                      openmpi-bin automake gfortran libexpat1-dev subversion libtool doxygen
+
+
+## on  Trusty Tahr - Ubuntu 14.04 LTS
+sudo apt-get install build-essential subversion libopenmpi-dev libfftw3-dev libtool bison \
+                      flex pkg-config automake libexpat1-dev gfortran openmpi-bin doxygen
+
+
 # Install Intel IPP
+#Here maybe you can refer <https://software.intel.com/en-us/articles/free_ipp> for a free licence.
+
+# Install openmpi or fftw if need
+
+# Install DiFX
+# 1. First modify the setup.bash, such as DIFXROOT,PGPLOTDIR,IPPROOT,MPICXX etc...
+source setup.bash
+./install-difx
+./install-difx --mk5daemon
+./install-difx --withmonitor
+./install-difx --withfb
+./install-difx --withhops
+./install-difx --withguiserver
